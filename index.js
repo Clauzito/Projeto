1449eed4ef085cda0356a72e0ac35bc0d56bac11
Claudecir <super.claau@hotmail.com>
2025-06-02 20:31:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+    
+    // pegando Html
+    const html = document.documentElement
+    // Fazendo a troca das classes Light/dark mode
+    html.classList.toggle('light')
+    //Pegar a img
+    const img = document.querySelector("#profile img")
+    // Subistituir a imagem
+    if(html.classList.contains('light')) {
+      // Se tiver light mode, adicionar a imagem light.
+        img.setAttribute('src', './assets/avatar-light.jpg')
+     } else {
+            // se não tiver light mode, manter a normal.
+        img.setAttribute('src', './assets/avatar.png')
+    } 
+
+   // Este codigo faz o mesmo que o outro só que com menas linahs de codigo.
+}
